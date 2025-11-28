fe719b377f1aaf953432b3bc253dd0d6f8e027d2
Abdul Wajid <waji0003@gmail.com>
2025-11-28 01:20:45 -0500

    Add screenshot of patient form

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const PatientSchema = new mongoose.Schema({
-  name: String,
+ firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   age: Number,
   gender: String,
-  email: String,
+  email: { type: String, required: true },
   phone: String
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Patient", PatientSchema);
